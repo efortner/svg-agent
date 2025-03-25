4bd5ec0c154b8f6f4b1f7a50a0861e6f8713ac66
Ethan Fortner <ethan@anaplian.io>
2025-03-24 20:33:01 -0500

    feat: finished agent but will require some infrastructure improvement for it to work correctly

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Ollama } from '@langchain/ollama';
+import { buildAgent } from './utilities/agent-factory';
+
+(async () => {
+  const model = new Ollama({
+    model: 'gemma3:27b',
+    baseUrl: 'http://localhost:11434',
+  });
+  const agent = await buildAgent(
+    model,
+    128_000,
+    'You are a graphic designer, and your goal is to create an SVG image of a PIXEL ART CAT using the svg() action. It must be highly detailed.' +
+      ' Use multiple iterations to fix mistakes and improve detail. You will alternate between using the think() and ' +
+      ' svg() actions. Use the think() action to plan and the svg() action to render your SVG.',
+  );
+  process.on('SIGCONT', () => agent.shutdown());
+  await agent.run();
+})();
--- src/utilities/agent-factory.ts
+++ src/utilities/agent-factory.ts
@@ -2,10 +2,12 @@ import {
   AgentBuilder,
   AnaplianAgent,
   HistoryContextProvider,
+  ThinkAction,
 } from '@anaplian/core';
 import { BaseLLM } from '@langchain/core/language_models/llms';
 import { BaseChatModel } from '@langchain/core/language_models/chat_models';
 import { RenderSvg } from '../contexts/render-svg';
+import { Svg } from '../actions/svg';
 
 export const buildAgent = (
   model: BaseLLM | BaseChatModel,
@@ -19,4 +21,12 @@ export const buildAgent = (
     .setContextWindowSize(contextWindowSize)
     .addContextProvider(new HistoryContextProvider({}), 30)
     .addContextProvider(new RenderSvg(), 70)
+    .addAction(new ThinkAction())
+    .addAction(new Svg())
+    .setOn('afterIterationEnd', async (context) =>
+      console.info(JSON.stringify(context, null, 2)),
+    )
+    .setOn('beforeShutdown', async () =>
+      console.info('RECEIVED SHUTDOWN SIGNAL'),
+    )
     .build();
